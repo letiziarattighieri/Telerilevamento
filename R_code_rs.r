f8744310b81803ff243e6b3eefa89dab56338f92
letiziarattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2022-03-10 17:53:17 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,21 @@
 # Questo è il primo script che useremo a lezione
 
+#install.packages("raster")
 library(raster)
+
+#settaggio cartella di lavoro
+setwd("C:/lab/")
+
+#importiamo dati
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+#per vedere l'immagine, la plottiamo con funzione plot
+plot(l2011)
+
+#cambiamo il colore della legenda. Il 100 indica il numero di colori intermedi tra i 3 che ho inserito nella nuova legenda
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+
+#dopo aver cambiato il colore della legenda proseguo plottando le nuove immagini
+plot(l2011, col=cl)
+
