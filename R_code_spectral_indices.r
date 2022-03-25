ad0d776765d02fa6667ff2a481080eaa7a446a95
letiziarattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2022-03-25 16:36:37 +0100

    Create R_code_spectral_indices.r

---
+++ R_code_spectral_indices.r
@@ -0,0 +1,34 @@
+# 25 marzo
+
+# Usiamo gli indici di vegetazione
+library(raster)
+setwd("C:/lab/")
+
+# Import the first file -> defor1_.jpg -> give it the name l1992
+l1992 <- brick("defor1_.jpg")
+l1992
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
