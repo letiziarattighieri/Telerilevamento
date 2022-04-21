e80009aeb29b0727e222c5a0e88b262473412b83
Letizia Rattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2022-04-21 17:57:53 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -24,5 +24,38 @@ plot(soc$map, col=cl)
 
 
 
+# 21 aprile 
+# Riprendiamo la classificazione di immagini con il Grand Canyon
+
+library(raster)
+library(RStoolbox)
+
+setwd("C:/lab/")
+
+# Importiamo l'immagine satellitare che classificheremo. E' un'immagine a 8 bit, con 3 bande
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+# Plottiamo l'immagine e si vede con i colori che vede l'occhio umano perchè le bande sono in ordine R, G, B
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# Classifichiamo l'immagine
+gcclass2 <- unsuperClass(gc, nClasses=2)
+plot(gcclass2$map) 
+
+# Exercise: classify the map with 4 classes
+gcclass4 <- unsuperClass(gc, nClasses=4)
+plot(gcclass4$map, col=cl)
+cl <- colorRampPalette(c("yellow", "red", "blue", "black"))(100)
+
+# Compare the classified map with the original set
+par(mfrow=c(2,1))
+plot(gcclass4$map, col=cl)
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+
+
 
 
