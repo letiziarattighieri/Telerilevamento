b2b936588d0cb64126eca58c4e5661a548df9ee2
Letizia Rattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2022-05-03 10:43:54 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -4,24 +4,34 @@
 library(raster)
 library(RStoolbox) # for image viewing and variability calculation
 library(ggplot2) # for ggplot plotting
-library(patchwork)
+library(patchwork) # multiframe with ggplot
 library(viridis)
 setwd("C:/lab/")
 
-# Excercise: import the Similaun Glacier image
+# Excercise: import the Similaun Glacier image. Uso brick perchè è un'immagine multispettrale
+# e voglio caricare tutte le bande contemporaneamente
+# band1 = NIR
+# band2 = red
+# band3 = green
 sent <- brick("sentinel.png")
 
 # Exercise: plot the image by the ggRGB function
 ggRGB(sent, 1, 2, 3, stretch="lin")
+# oppure
+ggRGB(sent, 1, 2, 3)
 # Con ggRGB lo stretch non è obbligatorio, invece va messo per forza con plotRGB
 
-# Mettiamo l'infrarosso nella componente G
+# Mettiamo l'infrarosso nella componente G per vedere come cambia l'immagine
 ggRGB(sent, 2, 1, 3)
 
 # Exercise: plot the two graphs one beside the other. Possibile grazie a patchwork
 g1 <- ggRGB(sent, 1, 2, 3, stretch="lin")
 g2 <- ggRGB(sent, 2, 1, 3, stretch="lin")
 g1+g2
+# Exercise: plot one graph on top of the other
+g1/g2
+# Per mettere 4 plot insieme si può fare:
+(g1 + g2) / (g1 + g2)
 
 # Calculation of variability over NIR
 nir <- sent[[1]]
