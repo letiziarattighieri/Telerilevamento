c785da587e49ba6aa4f8f29b9b8a4c80d29c03d5
Letizia Rattighieri <100956798+letiziarattighieri@users.noreply.github.com>
2022-06-04 21:36:45 +0200

    Create R_code_exam.r

---
+++ R_code_exam.r
@@ -0,0 +1,2 @@
+# Analisi e confronto di due immagini landsat: una acquisita nel 2013 e l'altra nel 2021
+# Le aree interessate comprendono Purto Montt, il Lago Llanquihue e il Parque Nacional Alerce Andino (Cile)
